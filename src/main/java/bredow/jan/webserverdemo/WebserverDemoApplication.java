c19f37f553082b74e317f540a3b7a38fdedafc4f
Jan Christopher Bredow <jan.bredow@epeu.dev>
2021-06-10 06:37:15 +0200

    inline too-long-code...

--- src/main/java/bredow/jan/webserverdemo/WebserverDemoApplication.java
+++ src/main/java/bredow/jan/webserverdemo/WebserverDemoApplication.java
@@ -67,7 +67,8 @@ public class WebserverDemoApplication {
 
     private ConsoleLogger printConfigurationProperties(
         DatabaseConfiguration databaseConfiguration) {
-      return LOG.logSpaced("Hostname:  \"" + databaseConfiguration.databaseHost() + "\"", 4)
+      return LOG
+          .logSpaced("Hostname:  \"" + databaseConfiguration.databaseHost() + "\"", 4)
           .logSpaced("Port:      \"" + databaseConfiguration.databasePort() + "\"", 4)
           .logSpaced("Database:  \"" + databaseConfiguration.databaseName() + "\"", 4)
           .appendLogger(printDatabaseUserCredentials(databaseConfiguration));
@@ -76,66 +77,26 @@ public class WebserverDemoApplication {
     private ConsoleLogger printDatabaseUserCredentials(
         DatabaseConfiguration databaseConfiguration) {
       var databaseCredentials = databaseConfiguration.databaseCredentials();
-      return LOG.logSpaced("Username:  \"" + databaseCredentials.username() + "\"", 4)
+      return LOG
+          .logSpaced("Username:  \"" + databaseCredentials.username() + "\"", 4)
           .logSpaced("Password:  \"" + "*".repeat(databaseCredentials.password().length()), 4);
     }
 
     private ConsoleLogger printPoolProperties(DatabasePoolConfiguration poolConfiguration) {
-      return LOG.logSpaced(
-              "Application-Name:                   \"" + poolConfiguration.applicationName() + "\"",
-              4)
-          .logSpaced(
-              "Cache Server Configuration:         \""
-                  + poolConfiguration.cacheServerConfiguration()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Cache Prepared Statements:          \"" + poolConfiguration.cachePrepStmts() + "\"",
-              4)
-          .logSpaced(
-              "Cache RestultSetMetadata:           \""
-                  + poolConfiguration.cacheResultSetMetadata()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Cache Size for Statements:          \""
-                  + poolConfiguration.prepStmtCacheSize()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Cache Size for Prepared Statements: \""
-                  + poolConfiguration.prepStmtCacheSqlLimit()
-                  + "\"",
-              4)
+      return LOG
+          .logSpaced("Application-Name:                   \"" + poolConfiguration.applicationName() + "\"", 4)
+          .logSpaced("Cache Server Configuration:         \"" + poolConfiguration.cacheServerConfiguration() + "\"", 4)
+          .logSpaced("Cache Prepared Statements:          \"" + poolConfiguration.cachePrepStmts() + "\"", 4)
+          .logSpaced("Cache RestultSetMetadata:           \"" + poolConfiguration.cacheResultSetMetadata() + "\"", 4)
+          .logSpaced("Cache Size for Statements:          \"" + poolConfiguration.prepStmtCacheSize() + "\"", 4)
+          .logSpaced("Cache Size for Prepared Statements: \"" + poolConfiguration.prepStmtCacheSqlLimit() + "\"", 4)
           .space()
-          .logSpaced(
-              "Maximum Pool-Size:                  \"" + poolConfiguration.maximumPoolSize() + "\"",
-              4)
-          .logSpaced(
-              "Use Server Prepared Statements?     \""
-                  + poolConfiguration.useServerPrepStmts()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Use Local Session State?            \""
-                  + poolConfiguration.useLocalSessionState()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Rewrite Batch Statements?           \""
-                  + poolConfiguration.rewriteBatchedStatements()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Elide Set Auto-Commits?             \""
-                  + poolConfiguration.elideSetAutoCommits()
-                  + "\"",
-              4)
-          .logSpaced(
-              "Maintain Time-Stats?                \""
-                  + poolConfiguration.maintainTimeStats()
-                  + "\"",
-              4);
+          .logSpaced("Maximum Pool-Size:                  \"" + poolConfiguration.maximumPoolSize() + "\"", 4)
+          .logSpaced("Use Server Prepared Statements?     \"" + poolConfiguration.useServerPrepStmts() + "\"", 4)
+          .logSpaced("Use Local Session State?            \"" + poolConfiguration.useLocalSessionState() + "\"", 4)
+          .logSpaced("Rewrite Batch Statements?           \"" + poolConfiguration.rewriteBatchedStatements() + "\"", 4)
+          .logSpaced("Elide Set Auto-Commits?             \"" + poolConfiguration.elideSetAutoCommits() + "\"", 4)
+          .logSpaced("Maintain Time-Stats?                \"" + poolConfiguration.maintainTimeStats() + "\"", 4);
     }
   }
 }
